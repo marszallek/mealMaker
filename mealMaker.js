67e71657f532241ccd4e0a8fa07658ad72e2f26d
marszallek <marszallek@vp.pl>
2019-01-06 05:16:37 +0100

    init

---
+++ mealMaker.js
@@ -0,0 +1,32 @@
+let menu = {
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+  },
+  	get courses() {
+      return {
+        appetizers: _courses.appetizers,
+        mains: _courses.mains,
+        desserts: _courses.desserts,
+      }
+    },
+    get apetizer(){
+      
+    },
+    set apetizer(apetizerIn){
+      
+    },
+    get mains(){
+      
+    },
+    set mains(mainsIn){
+      
+    },
+    get desserts(){
+      
+    },
+    set desserts(dessertsIn){
+      
+    },
+};
